a0bca793c9c75ff446b49dff113a3cd52afa15f6
jaiderosorio311-cmd <jaiderosorio311@gmail.com>
2026-05-29 12:09:05 -0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,11 +1,16 @@
-
-
-const toggle = document.getElementById("menu-toggle");
+const menuToggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
-toggle.addEventListener("click", ()=>{
-
-    menu.classList.toggle("active");
-
+// Abrir / cerrar con hamburguesa
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  menuToggle.classList.toggle("active");
 });
 
+// Cerrar al hacer clic en cualquier opción
+document.querySelectorAll(".menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+    menuToggle.classList.remove("active");
+  });
+});
